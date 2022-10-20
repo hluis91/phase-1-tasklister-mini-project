c40fdcd05dd7d95438779352ef43cbcd23485ac0
Luis Hernandez <lshrnndz6@gmail.com>
2022-10-20 13:05:19 -0700

    Pushing project code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("#create-task-form");
+
+  form.addEventListener('submit', (e)=> {
+    e.preventDefault()
+    createElement(e.target.children[1].value);
+    form.reset();
+  });
 });
+
+function createElement(create){
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', deleteTask)
+  btn.textContent = 'X';
+  li.textContent = `${create} `;
+  li.appendChild(btn)
+  console.log(li);
+  document.querySelector('#tasks').appendChild(li);
+};
+
+function deleteTask(e){
+  e.target.parentNode.remove();
+};
